b58466e5dfdc3fae53aaee37e7491e77a35d391a
lepture <lepture@me.com>
2011-10-15 16:52:38 +0800

    setup changes , more readme

--- README.rst
+++ README.rst
@@ -1,4 +1,8 @@
 Felix Felicis
 ==============
 
-Felix Felicis, also known as *liquid luck*.
+Felix Felicis (aka ``liquidluck``) is a simple lightweight static blog generator. It support markup of ``restructedText`` and ``markdown`` .
+
+Getting Started
+----------------
+Take a look at `Wiki <https://github.com/lepture/liquidluck/wiki>`_
--- setup.py
+++ setup.py
@@ -3,19 +3,28 @@
 
 from setuptools import setup
 
+version = '0.1'
+
 setup(
-    name='liquidluck',
-    version='1.0',
-    author='Hsiaoming Young',
-    author_email = 'sopheryoung@gmail.com',
+    name = 'liquidluck',
+    version = version,
+    author = 'Hsiaoming Young',
+    author_email = 'lepture@me.com',
     url = 'http://github.com/lepture/liquidluck',
-    packages=['liquidluck', 'liquidluck.writers', 'liquidluck.readers'],
-    description='a static weblog generator',
-    long_description='a static weblog generator',
-    license='BSD License',
+    packages = ['liquidluck', 'liquidluck.writers', 'liquidluck.readers'],
+    description = 'A lightweight static weblog generator',
+    license = 'BSD License',
     entry_points = {
         'console_scripts': ['liquidluck= liquidluck.cli:main'],
     },
     install_requires=['docutils', 'Jinja2', 'Pygments', 'markdown'],
     include_package_data = True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Programming Language :: Python',
+    ]
 )
