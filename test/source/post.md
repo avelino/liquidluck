0c0f2bafa0b17ff979f1e58989fadfcc31b5ad93
lepture <lepture@me.com>
2012-06-11 01:16:31 +0800

    add test for reader

---
+++ test/source/post.md
@@ -0,0 +1,18 @@
+-----------
+title: demo
+date: 2012-12-12
+tags: tag1, tag2
+-----------
+
+first line
+
+```python
+def codeblock():
+    return True
+```
+
+second line
+third line
+
+中文
+测试
---
+++ test/test_readers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os.path
+from liquidluck.readers.markdown import MarkdownReader
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+
+
+class TestMarkdownReader(object):
+    def setUp(self):
+        path = os.path.join(ROOT, 'source/post.md')
+        self.reader = MarkdownReader(path)
+        self.post = self.reader.render()
+
+    def test_title(self):
+        assert self.post.title == 'demo'
+
+    def test_tags(self):
+        assert self.post.tags == ['tag1', 'tag2']
+
+    def test_public(self):
+        assert self.post.public is True
+
+    def test_author(self):
+        assert self.post.author is None
+
+    def test_pygments(self):
+        assert 'highlight' in self.post.content
