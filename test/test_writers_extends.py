c7c282ec0bd3ba88177980ec318857d52e098028
lepture <lepture@me.com>
2012-06-12 20:14:59 +0800

    test for tagwriter

--- test/test_writers_extends.py
+++ test/test_writers_extends.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
 import os
-from liquidluck.writers.extends import YearWriter
+from liquidluck.writers.extends import YearWriter, TagWriter
 from liquidluck.options import settings
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-class TestArchiveWriter(object):
+class TestYearWriter(object):
     def test_run(self):
         writer = YearWriter()
         writer.run()
         f = os.path.join(os.getcwd(), settings.deploydir, '2012/index.html')
+        assert os.path.exists(f)
+
+
+class TestTagWriter(object):
+    def test_run(self):
+        writer = TagWriter()
+        writer.run()
+        f = os.path.join(
+            os.getcwd(), settings.deploydir, 'tags/tag1/index.html'
+        )
         assert os.path.exists(f)
