88fef64a49f052a68133bc49ef5c82624a3568e2
lepture <lepture@me.com>
2012-06-13 21:18:39 +0800

    start theme

--- liquidluck/_themes/default/config.py
+++ liquidluck/_themes/default/config.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
 theme = {
-    'name': 'Felix Felicis',
+    'name': 'moment',
     'link': 'http://project.lepture.com/liquidluck/',
     'version': '0.1',
 
     'author_name': 'lepture',
     'author_website': 'http://lepture.com',
     'author_email': 'lepture@me.com',
+}
+
+
+#: custom configuration
+nav_items = [
+    ('Home', '/'),
+    ('About', '/about.html'),
+]
+
+support = {
+    'disqus': None,
+    'github': None,
+    'twitter': None,
+    'analytics': None,  # google analytics ID
 }
