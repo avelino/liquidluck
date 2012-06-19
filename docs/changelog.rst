5a73a86eb3b0411d90a5a4a39c240b92df46b921
lepture <lepture@me.com>
2012-06-19 11:05:57 +0800

    version 1.1

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -3,6 +3,13 @@ Changelog
 
 All history since the new Felix Felicis are listed here:
 
+Version 1.1
+-------------
+
+Released on Jun 19th 2012.
+
++ search and install theme available
++ bugfix issue#20
 
 Version 1.0
 -------------
--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     install_requires=install_requires,
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Console',
         'License :: OSI Approved :: BSD License',
         'Operating System :: MacOS',
