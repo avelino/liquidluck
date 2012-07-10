7e7825eff901b83c1e024356ad9085397663c79e
lepture <lepture@me.com>
2012-07-10 22:21:57 +0800

    github api is not stable, don't install a theme

--- tests/test_tools_theme.py
+++ tests/test_tools_theme.py
@@ -11,9 +11,3 @@ def test_search():
 def test_search_keyword():
     search('moment')
     search('liquidluck')
-
-
-def test_install():
-    install('liquidluck')
-    install('moment')
-    assert os.path.exists(os.path.join(ROOT, '_themes/moment'))
