d80a6a640622454388eaaabe682eb62512778819
Hsiaoming Yang <me@lepture.com>
2013-08-05 15:02:42 +0800

    add cache system

---
+++ liquidluck/cache.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+"""
+    liquidluck.cache
+    ~~~~~~~~~~~~~~~~
+
+    Cache system for liquidluck.
+
+    :copyright: (c) 2013 by Hsiaoming Yang
+"""
+
+import os
+import shutil
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+class Cache(object):
+    """Cache for storing data.
+
+    :param cachedir: where to storing the data, if no value passed,
+                     use memory.
+    """
+
+    def __init__(self, cachedir=None):
+        if cachedir and not os.path.exists(cachedir):
+            os.makedirs(cachedir)
+        self._cachedir = cachedir
+        self._memcache = {}
+
+    def has(self, key):
+        """Detecting if the cache exists.
+
+        :param key: key of the cache.
+        """
+        if not self._cachedir:
+            return key in self._memcache
+        return os.path.exists(os.path.join(self._cachedir, key))
+
+    def get(self, key):
+        """Get the data with the given key.
+
+        :param key: key of the cache.
+        """
+        if not self._cachedir:
+            return self._memcache.get(key)
+        cachefile = os.path.join(self._cachedir, key)
+        if not os.path.exists(cachefile):
+            return None
+        with open(cachefile) as f:
+            return pickle.load(f)
+
+    def set(self, key, value):
+        """Storing the data with the given key.
+
+        :param key: key of the cache.
+        :param value: data to be stored in the cache.
+        """
+        if not self._cachedir:
+            self._memcache[key] = value
+            return self
+        cachefile = os.path.join(self._cachedir, key)
+        with open(cachefile, 'wb') as f:
+            pickle.dump(value, f)
+            return self
+
+    def clear(self, key):
+        """Delete the cache data with the given key.
+
+        :param key: key of the cache.
+        """
+        if not self._cachedir:
+            if key in self._memcache:
+                return self._memcache.pop(key)
+
+        cachefile = os.path.join(self._cachedir, key)
+        if os.path.exists(cachefile):
+            os.remove(cachefile)
+
+    def flush(self):
+        """Flush all cache data."""
+        if not self._cachedir:
+            self._memcache = {}
+        elif os.path.exists(self._cachedir):
+            shutil.rmtree(self._cachedir)
---
+++ tests/test_cache.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from liquidluck.cache import Cache
+import tempfile
+
+
+def test_memcache():
+    cache = Cache()
+    assert cache.get('foo') == None
+
+    cache.set('foo', 'foo')
+    assert cache.has('foo')
+
+    assert cache.get('foo') == 'foo'
+
+    cache.set('bar', 'bar')
+    cache.set('baz', 'baz')
+
+    assert cache.has('bar')
+    cache.clear('bar')
+    assert not cache.has('bar')
+
+    cache.flush()
+    assert not cache.has('baz')
+    assert not cache.has('foo')
+
+
+def test_filecache():
+    cachedir = tempfile.mkdtemp()
+    cache = Cache(cachedir)
+    assert cache.get('foo') == None
+
+    cache.set('foo', 'foo')
+    assert cache.has('foo')
+
+    assert cache.get('foo') == 'foo'
+
+    cache.set('bar', 'bar')
+    cache.set('baz', 'baz')
+
+    assert cache.has('bar')
+    cache.clear('bar')
+    assert not cache.has('bar')
+
+    cache.flush()
+    assert not cache.has('baz')
+    assert not cache.has('foo')
