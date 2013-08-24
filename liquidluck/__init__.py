835d1a6d9286d58cdad2361a57eb883494243e87
Hsiaoming Yang <me@lepture.com>
2013-08-24 10:59:10 +0800

    A quick fix for #106 on default theme

--- liquidluck/__init__.py
+++ liquidluck/__init__.py
@@ -29,6 +29,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '3.8'
-__author__ = 'Hsiaoming Yang <lepture@me.com>'
+__version__ = '3.8.1'
+__author__ = 'Hsiaoming Yang <me@liquidluck.com>'
 __homepage__ = 'http://lab.lepture.com/liquidluck/'
